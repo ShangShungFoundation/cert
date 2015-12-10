54860cf597b3ee110833d3d18c13e4df297ce20c
k1000 <selwak@gmail.com>
2015-12-10 18:21:13 +0100

    adds participant to courses admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 
-from models import Course
+from models import Course, Participant
 
 
 class CourseAdmin(admin.ModelAdmin):
     list_display = ("title", "begins", "organizer", "status")
 
 
-admin.site.register(Course, CourseAdmin)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ("person", "course", "payed", "status")
 
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Participant, ParticipantAdmin)
