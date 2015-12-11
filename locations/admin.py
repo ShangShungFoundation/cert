6ede61ac278a3e93647aac4a0cb1b1b1b55d9fc9
kamil <selwak@gmail.com>
2015-12-11 12:55:44 +0100

    establish Zone in locations

--- locations/admin.py
+++ locations/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from models import Location
+from models import Location, Zone
 
 
 class LocationAdmin(admin.ModelAdmin):
     list_display = ("name", "country")
 
-admin.site.register(Location, LocationAdmin)
 
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Zone)
--- locations/models.py
+++ locations/models.py
@@ -5,7 +5,15 @@ from django.db import models
 COUNTRIES = [(c.alpha2, c.name) for c in pycountry.countries]
 
 
+class Zone(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __unicode__(self):
+        return u"%s" % (self.name)
+
+
 class Location(models.Model):
+    zone = models.ForeignKey(Zone)
     name = models.CharField(max_length=250)
     place = models.TextField()
     country = models.CharField(
