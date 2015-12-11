002bbe4f98705e5a3f0fab699d858b4b911326fd
kamil <selwak@gmail.com>
2015-12-11 12:52:44 +0100

    basic admin setup for courses

--- courses/admin.py
+++ courses/admin.py
@@ -1,15 +1,39 @@
 from django.contrib import admin
 
-from models import Course, Participant
+
+from models import EducationalProgramme
+from models import ParticipantGroup, Participant
+from models import Course, Fee, File, Communication
 
 
-class CourseAdmin(admin.ModelAdmin):
-    list_display = ("title", "begins", "organizer", "status")
+class EducationalProgrammeAdmin(admin.ModelAdmin):
+    list_display = ("title", "institution")
 
 
 class ParticipantAdmin(admin.ModelAdmin):
     list_display = ("person", "course", "payed", "status")
 
 
-admin.site.register(Course, CourseAdmin)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("educational_programme", "location", "begins", "status")
+
+
+class FeeAdmin(admin.ModelAdmin):
+    list_display = ("programme", "participant_group", "zone")
+
+
+class FileAdmin(admin.ModelAdmin):
+    list_display = ("course", "file", "type")
+
+
+class CommunicationAdmin(admin.ModelAdmin):
+    list_display = ("course", "subject", "sent_at")
+
+
+admin.site.register(EducationalProgramme, EducationalProgrammeAdmin)
+admin.site.register(ParticipantGroup)
 admin.site.register(Participant, ParticipantAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Fee, FeeAdmin)
+admin.site.register(File, FileAdmin)
+admin.site.register(Communication, CommunicationAdmin)
