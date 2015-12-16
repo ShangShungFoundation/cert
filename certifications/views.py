f1f71b615b738a8f50e6498b74f460855bde2679
kamil <selwak@gmail.com>
2015-12-16 12:23:12 +0100

    db updates

--- certifications/views.py
+++ certifications/views.py
@@ -14,6 +14,7 @@ def certifications(request):
     return render(request, "certifications/certifications.html",
         dict(certifications=certifications) )
 
+
 def certification(request, object_id):
     certification = CertificationProgramme.objects.get(pk=object_id)
     cert_verify_form = CertVerifyForm(request.GET)
@@ -21,10 +22,12 @@ def certification(request, object_id):
         dict(certification=certification,
             cert_verify_form=cert_verify_form) )
 
+
 def certificate(request, email):
     cert = Certificate.objects.get(person__email=email)
     return render(request, "certifications/certificate.html",
         dict(cert=cert) )
+
 
 def certificates(request, certification_id):
     search_str = request.GET.get("search")
