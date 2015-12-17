c9667b1ea55f8a122f51400fc93e9ee12f2460ae
kamil <selwak@gmail.com>
2015-12-17 12:37:44 +0100

    add courses

--- cert/urls.py
+++ cert/urls.py
@@ -20,5 +20,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^educational-programmes/', include("courses.urls")),
     url(r'^certifications/', include("certifications.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
