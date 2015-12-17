b64ffa1ce366d65b5442b17a6a86844868c7a8bb
k1000 <selwak@gmail.com>
2015-12-17 17:08:18 +0100

    adds fees in Course view

--- courses/views.py
+++ courses/views.py
@@ -1,33 +1,36 @@
 from django.shortcuts import render
 
-from models import EducationalProgramme, Course
+from models import EducationalProgramme, Course, Fee
 
 
 def programmes(request):
     programmes = EducationalProgramme.active.all()
     return render(request, "courses/programmes.html",
-        dict(programmes=programmes) )
+        dict(programmes=programmes))
 
 
 def programme(request, object_id):
     programme = EducationalProgramme.active.get(pk=object_id)
     return render(request, "courses/programme.html",
-        dict(programme=programme) )
+        dict(programme=programme))
 
 
 def open_courses(request):
     courses = Course.objects.opened()
     return render(request, "courses/open_courses.html",
-        dict(courses=courses) )
+        dict(courses=courses))
 
 
 def course(request, object_id):
     course = Course.objects.get(pk=object_id)
+    fees = Fee.objects.filter(
+        programme_id=course.educational_programme.id,
+        zone_id=course.location.zone_id)
     return render(request, "courses/course.html",
-        dict(course=course) )
+        dict(course=course, fees=fees))
 
 
 def recruit(request, object_id):
     course = Course.active.get(pk=object_id)
     return render(request, "courses/course.html",
-        dict(courses=courses) )
+        dict(courses=courses))
